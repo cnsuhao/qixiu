a25025edb6e126e92aa45790d6fd0dd86376ff18
jhin <flyingmr@163.com>
2017-07-15 15:49:12 +0800

    V-1

---
+++ yii2/advanced/backend/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * @author  Jin <[flyingMr@163.com]>
+	 * @date 
+	 * @file  	
+	 * @brief   [测试数据]
+	 * @version [PHP-5.6]
+	 */
+
+	namespace backend\controllers;
+
+	use Yii;
+	use yii\web\Controller;
+
+	class HomeController extends Controller {
+		public function actionIndex() {
+			return $this->renderPartial('index');
+		}
+	}
